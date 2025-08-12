5caf53630ac0bb4fe42188854ff189ffd28ad0f2
Daulton Truett <dtruett45@gmail.com>
2025-08-11 23:59:02 -0600

    Registrerd core app into INSTALLED_APPS and defined user model since it extended the django default user model..

--- creator_boards/settings.py
+++ creator_boards/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Custom apps
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -89,6 +91,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'core.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
